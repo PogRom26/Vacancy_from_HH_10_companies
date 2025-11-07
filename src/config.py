616a2f58a784c8f4c8644b65e5aa54399ec90fd5
Roman Pogoreltcev <romzes26@yandex.ru>
2025-11-07 10:44:14 +0700

    первичное наполнение

---
+++ src/config.py
@@ -0,0 +1,20 @@
+# Конфигурационные параметры
+DB_NAME = "hh_vacancies"
+DB_USER = "postgres"
+DB_PASSWORD = "your_password"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+# Список компаний для сбора данных
+COMPANIES = [
+    {"id": 1740, "name": "Яндекс"},
+    {"id": 15478, "name": "VK"},
+    {"id": 3529, "name": "Сбер"},
+    {"id": 4181, "name": "2ГИС"},
+    {"id": 1057, "name": "Касперский"},
+    {"id": 907345, "name": "Тинькофф"},
+    {"id": 4934, "name": "Билайн"},
+    {"id": 39305, "name": "Газпром нефть"},
+    {"id": 64174, "name": "СберТех"},
+    {"id": 2324020, "name": "Ozon"}
+]
